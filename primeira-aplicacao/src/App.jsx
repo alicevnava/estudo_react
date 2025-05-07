691c94d25936c0ac294163fb8ebececab9544bd7
alicevnava <vivasn.alice@gmail.com>
2025-05-07 12:31:47 -0300

    contunuação

--- primeira-aplicacao/src/App.jsx
+++ primeira-aplicacao/src/App.jsx
@@ -1,16 +1,31 @@
 
-import './App.css'
+// PARTE JS
+//function App() {
+const App = () => {
+  const x = 10
 
-function App() {
+  const funcaoY = () => {
+    console.log('Aperte o botão', x)
+  }
 
+  funcaoY()
 
+//PARTE HTML
   return (
+    //precisa de um elemento pai - fragment, pode ser vazio, uma div ou um componente
     <>
       <div>
         <h1>Olá Mundo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit vitae eligendi odit eveniet aliquam reiciendis ut voluptates sit, odio quidem accusamus provident aperiam nobis quisquam. Voluptas in laudantium molestiae maxime?</p>
+        <button>Aperte</button>
       </div>
     </>
   )
 }
 
+// default - exporta um componente padrão
 export default App
+//export {App} - exporta um componente nomeado, evitando erros de exportação 
+
+// se tivesse um funtio App2 () {..}, teria ue exportar como:
+// export {App, App2 }
--- primeira-aplicacao/src/main.jsx
+++ primeira-aplicacao/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+//paar exportar mais de um componente, usar: import { App, App2 } from './App.jsx'
+//import {App} from './App.jsx' // importando o componente App, evitando erro de importação
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
